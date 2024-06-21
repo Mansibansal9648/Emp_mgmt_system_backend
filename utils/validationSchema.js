9da8a49280b8651aafb22638c1dd8ef8eed987ce
mansibansal9648 <mansibansal12345@gmail.com>
2024-06-21 21:49:50 +0530

    minor issues fix in validation create employee

--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -18,8 +18,10 @@ const createEmployeeSchema = [
     .notEmpty()
     .withMessage("Phone number is required field")
     .bail()
-    .matches(/^\d{10}$/)
-    .withMessage("Phone number should contain 10 digits"),
+    // .isMobilePhone().withMessage("Invalid phone number"),
+    .matches(/^[6789]\d{9}$/)
+    // .matches(/^\d{10}$/)
+    .withMessage("Invalid mobile number"),
   body("designation")
     .notEmpty()
     .withMessage("Designation is required field")
@@ -38,22 +40,29 @@ const createEmployeeSchema = [
     .withMessage("Salary is required field")
     .bail()
     .matches(/^[0-9]{4,15}$/)
-    .withMessage("Invalid salary format"),
+    .withMessage("Invalid salary format")
+    .bail()
+    .custom(async (value) => {
+      if (value < 1000) {
+        throw new Error("Salary should be greater than 1000");
+      }
+    }),
   body("date_of_joining")
     .notEmpty()
     .withMessage("Date of joining is required field")
     .bail()
-    .isISO8601()
+    .isDate({ format: "YYYY-MM-DD", strictMode: true })
+    // .isISO8601()
     .withMessage("Invalid date format")
     .bail()
-    .custom(async(value) => {
-        const date = new Date(value);
-        //  console.log(date)
-        const today = new Date();
-        // console.log(date, today);
-        if (date > today) {
-         throw new Error("Date of joining cannot be in the future");
-        }
+    .custom(async (value) => {
+      const date = new Date(value);
+      //  console.log(date)
+      const today = new Date();
+      // console.log(date, today);
+      if (date > today) {
+        throw new Error("Date of joining cannot be in the future");
+      }
     }),
 ];
 
