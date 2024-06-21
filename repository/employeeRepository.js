120ebd8993e73eea43d86ccfc6047648d94ce45c
mansibansal9648 <mansibansal12345@gmail.com>
2024-06-21 22:10:00 +0530

    minor issues fix in validation repository create employee

--- repository/employeeRepository.js
+++ repository/employeeRepository.js
@@ -1,26 +1,34 @@
 import { Employee } from "../schemas/employeeSchema.js";
 
-const createNewEmployee = async(data) => {
-//   return new Promise(async (resolve, reject) => {
-    try {
-        const existedEmployee = await Employee.findOne({  email: data.email 
-        });
-
-      if (existedEmployee && existedEmployee.email === data.email) {
-          throw new Error("Email Already Exists");
-        } else {
+const createNewEmployee = async (data) => {
+  //   return new Promise(async (resolve, reject) => {
+  try {
+    const existedEmployee = await Employee.findOne({
+      $or: [{ email: data.email }, { phone: data.phone }],
+    });
+    if (
+      existedEmployee &&
+      existedEmployee.email === data.email &&
+      existedEmployee.phone === data.phone
+    ) {
+      throw new Error("Email and Phone no already exists");
+    } else if (existedEmployee && existedEmployee.email === data.email) {
+      throw new Error("Email already Exists");
+    } else if (existedEmployee && existedEmployee.phone === data.phone) {
+      throw new Error("Phone no already Exists");
+    } else {
       // console.log("yttyu", data);
       const employee = new Employee(data);
       //  const newUser=await User.insert(req)
       // console.log(employee);
       const newEmployee = await employee.save();
       // console.log(newEmployee);
-      return(newEmployee);
-        }
-    } catch (error) {
-      throw(error);
+      return newEmployee;
     }
-//   });
+  } catch (error) {
+    throw error;
+  }
+  //   });
 };
 
 const getAllEmployees = (data) => {};
